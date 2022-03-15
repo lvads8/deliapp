ac2d56688574b70c91651b868acc3d40f8d0d7f9
lvads8 <lvads8@inf.elte.hu>
2022-03-15 21:02:56 +0100

    Create timespan cubit

---
+++ lib/cubits/timespan_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TimespanCubitData {
+  final DateTime from;
+  final DateTime to;
+
+  const TimespanCubitData(this.from, this.to);
+
+  @override
+  String toString() {
+    return "TimespanCubitData(from: $from, to: $to)";
+  }
+}
+
+class TimespanCubit extends Cubit<TimespanCubitData> {
+  TimespanCubit() : super(getInitialDate());
+
+  static TimespanCubitData getInitialDate() {
+    final now = DateTime.now();
+
+    return TimespanCubitData(
+      DateTime(now.year, now.month, now.day, 0, 0),
+      DateTime(now.year, now.month, now.day, 23, 59),
+    );
+  }
+
+  setFrom(DateTime d) {
+    final data = TimespanCubitData(d, state.to);
+    emit(data);
+  }
+
+  setTo(DateTime d) {
+    final data = TimespanCubitData(state.from, d);
+    emit(data);
+  }
+}
