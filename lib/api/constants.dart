8cc5fef56cb5bbb970a5105a9a2c5c199787e429
lvads8 <lvads8@inf.elte.hu>
2022-03-20 01:03:16 +0100

    Add remaining API urls

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -12,3 +12,11 @@ const String getStartedTripsUlr =
 const String getHistoryUrl = baseUrl +
     "DeliveryMediumApp/deliverymedium/fmlm/mobile/dmhistory?pageSize=100";
 const String tripIdUrl = baseUrl + "TripApp/trip/lmfm/getStartedTripsByUserId";
+const String checkinUrl =
+    baseUrl + 'ShipmentApp/mobile/shipment/mile/checkin?path_param_url=1729';
+const String checkoutUrl =
+    baseUrl + 'ShipmentApp/mobile/shipment/mile/mark?path_param_url=1729';
+const String paymentUrl =
+    baseUrl + 'ShipmentApp/mobile/shipment/payment/v2?path_param_url=1729';
+const String ordersUrl = baseUrl +
+    'ShipmentApp/mobile/shipment/list?version=466&paramkey=com.loginext.tracknext';
