cd9419649440e63f3a374e11fa43744dab6d603f
lvads8 <lvads8@inf.elte.hu>
2022-03-29 15:26:40 +0200

    Improve UX of refresh indicator

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,7 +1,9 @@
+import 'package:declarative_refresh_indicator/declarative_refresh_indicator.dart';
 import 'package:deliapp/api/common.dart';
 import 'package:deliapp/api/orders.dart';
 import 'package:deliapp/cubits/auth_cubit.dart';
 import 'package:deliapp/cubits/orders_cubit.dart';
+import 'package:deliapp/cubits/refreshing_cubit.dart';
 import 'package:deliapp/screens/main_screen.dart';
 import 'package:deliapp/utils.dart';
 import 'package:flutter/material.dart';
@@ -13,90 +15,147 @@ class OrdersScreen extends StatelessWidget with MainScreenComponent {
   OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  List<WidgetBuilder> get actions => [];
+  String get title => 'Rendelések';
 
   @override
   Icon get icon => const Icon(Icons.directions_car);
 
   @override
-  String get title => 'Rendelések';
+  List<WidgetBuilder> get actions {
+    return [
+      (context) => BlocBuilder<OrdersCubit, OrdersResponse?>(
+            builder: (context, state) {
+              final orders = state?.orders;
+              if (orders == null || orders.isEmpty) {
+                return Container();
+              }
+
+              final pickups = orders.where((o) => !o.pickedUp).toList();
+              if (pickups.isEmpty) {
+                return Container();
+              }
+
+              final auth = context.read<AuthCubit>().state!.auth;
+
+              return IconButton(
+                icon: const Icon(Icons.archive),
+                onPressed: () async {
+                  if (!await Utils.promptUser(
+                    context,
+                    'Biztosan felveszed az összes rendelést?',
+                  )) {
+                    return;
+                  }
+
+                  context.read<RefreshingCubit>().refresh();
+
+                  await Future.forEach<Order>(
+                    pickups,
+                    (pickup) => Utils.tryRequest(
+                      context,
+                      () async {
+                        await pickup.checkin(auth);
+                        await pickup.checkout(auth);
+                      },
+                      null,
+                    ),
+                  );
+
+                  _onRefresh(context, auth);
+                },
+              );
+            },
+          ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     final auth = context.read<AuthCubit>().state!.auth;
 
-    return RefreshIndicator(
-      key: _refresher,
-      triggerMode: RefreshIndicatorTriggerMode.anywhere,
-      child: Scrollbar(
-        child: BlocBuilder<OrdersCubit, OrdersResponse?>(
-          builder: (context, state) {
-            if (state == null) {
-              return ListView(
-                children: [
-                  Container(
-                    height: 150,
-                  ),
-                  Text(
-                    'Húzd le a frissítéshez',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).hintColor,
+    return BlocBuilder<RefreshingCubit, bool>(
+      builder: (context, state) => DeclarativeRefreshIndicator(
+        key: _refresher,
+        refreshing: state,
+        onRefresh: () => _onRefresh(context, auth),
+        triggerMode: RefreshIndicatorTriggerMode.anywhere,
+        child: Scrollbar(
+          child: BlocBuilder<OrdersCubit, OrdersResponse?>(
+            builder: (context, state) {
+              if (state == null) {
+                return ListView(
+                  children: [
+                    Container(
+                      height: 150,
                     ),
-                  ),
-                ],
-              );
-            }
-
-            if (state.orders.isEmpty) {
-              return ListView(
-                children: [
-                  Container(
-                    height: 150,
-                  ),
-                  Text(
-                    'Nincs rendelés',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Theme.of(context).hintColor,
+                    Text(
+                      'Húzd le a frissítéshez',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Theme.of(context).hintColor,
+                      ),
                     ),
-                  ),
-                ],
-              );
-            }
-
-            return ListView.separated(
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.all(4),
-                  child: Padding(
-                    padding: const EdgeInsets.all(2),
-                    child: _buildOrderCardBody(context, state.orders[index]),
-                  ),
+                  ],
                 );
-              },
-              separatorBuilder: (_, __) => Container(height: 2),
-              itemCount: state.orders.length,
-            );
-          },
+              }
+
+              if (state.orders.isEmpty) {
+                return ListView(
+                  children: [
+                    Container(
+                      height: 150,
+                    ),
+                    Text(
+                      'Nincs rendelés',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Theme.of(context).hintColor,
+                      ),
+                    ),
+                  ],
+                );
+              }
+
+              return Padding(
+                padding: const EdgeInsets.only(top: 4),
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    final order = state.orders[index];
+                    return Card(
+                      margin: const EdgeInsets.all(4),
+                      child: Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: _buildOrderCardBody(context, order),
+                      ),
+                    );
+                  },
+                  // separatorBuilder: (_, __) => Container(height: 2),
+                  itemCount: state.orders.length,
+                ),
+              );
+            },
+          ),
         ),
       ),
-      onRefresh: () => _onRefresh(context, auth),
     );
   }
 
   Widget _buildOrderCardBody(BuildContext context, Order order) {
     return ListTile(
       title: Text(order.clientName),
+      minLeadingWidth: 20,
       leading: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(
             order.pickedUp ? Icons.directions_car : Icons.archive,
+            color: order.pickedUp ? Colors.blue : Colors.amber,
           ),
         ],
       ),
-      subtitle: Text('${order.city}, ${order.streetName}, ${order.apartment}'),
+      subtitle: Text(order.city != null
+          ? '${order.city}, ${order.streetName}, ${order.apartment}'
+          : order.address),
       trailing: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -112,29 +171,28 @@ class OrdersScreen extends StatelessWidget with MainScreenComponent {
         ],
       ),
       onTap: () async {
-        // if (await Navigator.pushNamed(
-        //       context,
-        //       '/order',
-        //       arguments: order,
-        //     ) ??
-        //     false) {
-        //   await _refresher.currentState!.show();
-        // }
-
-        await Navigator.pushNamed(
+        final result = await Navigator.pushNamed(
           context,
           '/order',
           arguments: order,
-        );
+        ) as bool?;
+
+        if (result ?? false) {
+          _onRefresh(context, context.read<AuthCubit>().state!.auth);
+        }
       },
     );
   }
 
   Future<void> _onRefresh(BuildContext context, Authentication auth) async {
+    context.read<RefreshingCubit>().refresh();
+
     await Utils.tryRequest(
       context,
       () => context.read<OrdersCubit>().refresh(auth),
       null,
     );
+
+    context.read<RefreshingCubit>().finish();
   }
 }
