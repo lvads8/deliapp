86dda08e15b00c9c505cd7f0fdd088c037b7db84
lvads8 <lvads8@inf.elte.hu>
2022-04-06 22:20:01 +0200

    Start rewrite

--- lib/cubits/selected_index_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class SelectedIndexCubit extends Cubit<int> {
-  SelectedIndexCubit() : super(0);
-
-  int get index => state;
-  set index(int value) => emit(value);
-}
