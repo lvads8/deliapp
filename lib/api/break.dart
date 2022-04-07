b739cf5570ddcb8dab3fec1e86e8504815f213f7
lvads8 <lvads8@inf.elte.hu>
2022-04-07 09:46:55 +0200

    Ad break api wrapper

---
+++ lib/api/break.dart
@@ -0,0 +1,21 @@
+import 'client.dart';
+import 'models/api_auth.dart';
+
+abstract class Break {
+  static const _endpoint =
+      'DeliveryMediumApp/deliverymedium/fmlm/mobile/mark/break';
+
+  static Future<void> setBreak(ApiAuth auth, bool status) async {
+    final request = ApiRequest(
+      _endpoint,
+      apiAuth: auth,
+      body: '{isOnBreakFl: $status}',
+    );
+
+    final client = ApiClient();
+    final response = await client.put(request);
+
+    if (response.isUnauthorized) throw UnauthorizedError();
+    if (!response.isSuccess) throw response.error;
+  }
+}
